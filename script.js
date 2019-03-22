4d42e348f0dd7b37ce36d0ae01477fac15f7bed3
YobraulyCruz <43036792+YobraulyCruz@users.noreply.github.com>
2019-03-22 08:58:48 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,65 @@
+$(".nando").click(function() {
+    $(".chapo").show();
+    });
+    
+$(".nando").click(function() {
+    $(".nando").hide();
+    });
+    
+$(".chapo").click(function() {
+    $(".nando").show();
+    });
+    
+$(".chapo").click(function() {
+    $(".chapo").hide();
+    });
+    
+$(".oof").click(function() {
+    $(".mode").fadeIn();
+    });
+    
+$(".oof").click(function() {
+    $(".sicko").fadeOut();
+    });
+    
+$(".dicko").click(function() {
+    $(".sicko").fadeIn();
+    });
+    
+$(".dicko").click(function() {
+    $(".mode").fadeOut();
+    });
+    
+$(".dicko").click(function() {
+    $(".dicko").fadeOut();
+    });
+    
+$(".dicko").click(function() {
+    $(".oof").fadeIn();
+    });
+    
+$(".oof").click(function() {
+    $(".dicko").fadeIn();
+    });
+    
+$(".oof").click(function() {
+    $(".oof").fadeOut();
+    });
+    
+$(".dicko").click(function() {
+    $(".mode").fadeOut();
+    });
+    
+$(".slide").click(function() {
+    $(".gru").slideDown();
+    });
+  
+  $(".shh").click(function() {
+    $(".secret").fadeIn();
+    });
+    
+    $(".shh").click(function() {
+    $(".shh").fadeOut();
+    });
+    
+    
